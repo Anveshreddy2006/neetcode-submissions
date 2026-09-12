c4404ee04c78619db27e2e2f0fec230641091e86
Anveshreddy2006 <padidamanveshreddy@gmail.com>
2026-09-13 01:29:23 +0530

    Add: make-sum-divisible-by-p - submission-0

---
+++ Data Structures & Algorithms/make-sum-divisible-by-p/submission-0.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int minSubarray(int[] nums, int p) {
+        long total = 0;
+        for (int num : nums) total += num;
+        int remain = (int) (total % p);
+        if (remain == 0)
+            return 0;
+
+        int res = nums.length;
+        long curSum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            curSum = (curSum + nums[i]) % p;
+            int prefix = (int) ((curSum - remain + p) % p);
+            if (map.containsKey(prefix)) {
+                res = Math.min(res, i - map.get(prefix));
+            }
+            map.put((int) curSum, i);
+        }
+
+        return res == nums.length ? -1 : res;
+    }
+}
