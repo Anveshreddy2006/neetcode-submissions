73d86f2040f6c24ed08c80f22d01cde75da28e8a
Anveshreddy2006 <padidamanveshreddy@gmail.com>
2026-09-19 00:07:14 +0530

    Add: custom-sort-string - submission-1

---
+++ Data Structures & Algorithms/custom-sort-string/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        HashSet<Character> set = new HashSet<>();
+
+        for (char ch : order.toCharArray()) {
+            set.add(ch);
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (set.contains(ch)) {
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+            } else {
+                sb.append(ch);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+
+        for (char ch : order.toCharArray()) {
+            if (map.containsKey(ch)) {
+                int si = map.get(ch);
+                for (int j = 0; j < si; j++) {
+                    ans.append(ch);
+                }
+            }
+        }
+
+        ans.append(sb);
+        return ans.toString();
+    }
+}
